dbda42c2858da40cf8db18b05a48b41794fe7cec
Orochimaru Nakamoto <brantleigh.bunting@gmail.com>
2018-03-03 17:20:28 -0500

    Made calendar more calendar like

--- src/js/components/presentational/DisplayCalendar.js
+++ src/js/components/presentational/DisplayCalendar.js
@@ -6,30 +6,87 @@ import PropTypes from "prop-types";
 /* Import Bootstrap Table and its CSS */
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
+
 var classes = [
 	{
       id: 1,
       name: "Class One",
-      time: 120
+      time: "1:20 PM"
   	}, 
   	{
       id: 2,
       name: "Class Two",
-      time: 800
+      time: "8:00 PM"
   	}
   ];
 
-export default class DisplayCalendar extends Component {
-	componentDidMount() {
+class CellHTML extends Component {
+	render() {
+		var show = this.props.show;
+		const Inner = (props) => {
 
+			if (show) {
+				return (
+					<div id="registered">
+						<p>Course: </p>
+				  		<p>Time: </p>
+				  		<p>Place: </p>
+				  	</div>
+				);
+				
+			} else {
+				return (<div></div>);
+			}
+		}
+
+		return (
+			<div>
+				<Inner />
+			</div>
+		);
 	}
+
+
+}
+
+CellHTML.propTypes = {
+	show: PropTypes.bool.isRequired,
+};
+
+CellHTML.defaultProps = {
+	show: false
+};
+
+function cellFormatter(cell, row, x) {
+	console.log(cell);
+	console.log(row);
+
+	/* NOTE: We should do something like this
+	 * to display the course information on our calendar
+	 */
+
+	if (row["id"] == 2) {
+		return (<CellHTML show />
+		);
+	} else {
+		return (<CellHTML />);
+	}
+
+	
+}
+
+
+
+export default class DisplayCalendar extends Component {
 	render() {
 		return (
 			<div>
 			<BootstrapTable data={classes} striped hover>
 
-				<TableHeaderColumn isKey dataField='id'></TableHeaderColumn>
-				<TableHeaderColumn>Monday</TableHeaderColumn>
+				<TableHeaderColumn isKey dataField='time'>Time</TableHeaderColumn>
+				<TableHeaderColumn dataField="name" dataFormat={cellFormatter}>
+					Monday
+				</TableHeaderColumn>
 				<TableHeaderColumn>Tuesday</TableHeaderColumn>
 				<TableHeaderColumn>Wednesday</TableHeaderColumn>
 				<TableHeaderColumn>Thursday</TableHeaderColumn>
@@ -41,3 +98,5 @@ export default class DisplayCalendar extends Component {
 		);
 	}
 }
+
+
